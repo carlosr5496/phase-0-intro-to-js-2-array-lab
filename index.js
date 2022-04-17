f63d6f5185757aebf2fd557d59dd075df758afdf
Ryan Carlos <ryancarlos925@gmail.com>
2022-04-16 18:34:28 -0700

    commit tree b4aa10c66bcfe194bb9182e81ad845848593f213

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+
+
+function destructivelyAppendCat() {
+    return cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    return cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop()
+}
+
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift()
+}
+
+
+function appendCat(){
+    const catsSpread = [...cats, "Broom"]
+    return catsSpread
+}
+
+
+function prependCat() {
+    const catSpread = ["Arnold", ...cats];
+    return catSpread
+}
+
+function removeLastCat() {
+    return cats.slice(0,2)
+}
+
+function removeFirstCat() {
+    return cats.slice(1,3)
+}
